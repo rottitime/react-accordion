4ec1c22ed0fe789de3c5f816db59e1609267d575
Jas <webmaster@jaspaul.com>
2024-08-26 10:15:50 +0100

    latest options

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,35 +1,84 @@
-import { createElement, Fragment, useId } from 'react'
+import { createElement, Fragment, useId, useState } from 'react'
 import type { Props } from './types'
+import styled from 'styled-components'
+import Arrow from '../Arrow/Arrow'
 
-export default function Accordion({ headingLevel = 1, list, ...props }: Props) {
+const Wrapper = styled.div`
+  --ac-padding: 20px;
+  .heading {
+    display: flex;
+    border: 1px solid #ccc;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 10px;
+    margin: 0;
+    button {
+      border: 0;
+      padding: var(--ac-padding);
+      cursor: pointer;
+      background-color: transparent;
+      text-align: left;
+      flex-grow: 1;
+
+      &[aria-expanded='true'] + svg {
+        transform: rotate(180deg);
+      }
+    }
+    svg {
+      transition: all 0.3s ease-out;
+      margin-right: var(--ac-padding);
+    }
+  }
+
+  [role='region'] {
+    padding: var(--ac-padding);
+    background-color: #f9f9f9;
+  }
+`
+
+//document.getElementById('sect1').scrollHeight
+
+export default function Accordion({
+  headingLevel = 1,
+  defaultOpen = 0,
+  list,
+  ...props
+}: Props) {
+  const [opened, setOpened] = useState(defaultOpen)
   const id = useId()
 
   return (
-    <div {...props}>
-      {list.map(({ title, content, open }, i) => {
+    <Wrapper {...props}>
+      {list.map(({ title, content }, i) => {
         const headId = `${id}-${i}`
         const sectId = `sect${i + 1}`
-        const isOpen = !!open
+        const isOpen = opened === i
         return (
           <Fragment key={title}>
             {createElement(
               `h${headingLevel}`,
-              {},
-              <button
-                type="button"
-                aria-expanded={isOpen}
-                aria-controls={sectId}
-                id={headId}
-              >
-                {title}
-              </button>
+              { className: 'heading' },
+              <>
+                <button
+                  type="button"
+                  aria-expanded={isOpen}
+                  aria-controls={sectId}
+                  id={headId}
+                  onClick={() => {
+                    setOpened(i)
+                  }}
+                >
+                  {title}
+                </button>
+                <Arrow />
+              </>
             )}
             <div id={sectId} role="region" aria-labelledby={headId} hidden={!isOpen}>
-              {content}
+              <div>{content}</div>
             </div>
           </Fragment>
         )
       })}
-    </div>
+    </Wrapper>
   )
 }
--- src/components/Accordion/types.d.ts
+++ src/components/Accordion/types.d.ts
@@ -3,10 +3,10 @@ import type { ComponentProps, ReactNode } from 'react'
 type List = {
   title: string
   content: ReactNode
-  open?: boolean
 }
 
 export type Props = {
   headingLevel?: 1 | 2 | 3 | 4 | 5 | 6
   list: List[]
+  defaultOpen?: number
 } & ComponentProps<'div'>
