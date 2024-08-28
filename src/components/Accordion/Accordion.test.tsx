4d13b9c75d64b3a1200d9e6618e93e4b36085855
Jas <webmaster@jaspaul.com>
2024-08-28 10:46:37 +0100

    test

--- src/components/Accordion/Accordion.test.tsx
+++ src/components/Accordion/Accordion.test.tsx
@@ -1,42 +1,36 @@
-// import { render, screen } from '@testing-library/react'
-// import Details from './Accordion'
+import { render, screen, fireEvent } from '@testing-library/react'
+import Accordion from './Accordion'
 
-describe('Details', () => {
-  it('renders the summary', () => {
-    expect(true).toBeTruthy()
+const list = [
+  { title: 'Title 1', content: 'Content 1' },
+  { title: 'Title 2', content: 'Content 2' },
+  { title: 'Title 3', content: 'Content 3' }
+]
+
+describe('Accordion', () => {
+  it('renders the accordion with default open item', () => {
+    render(<Accordion list={list} defaultOpen={1} />)
+
+    expect(screen.getByText('Content 1')).not.toBeVisible()
+
+    // Assert that the second item is open by default
+    expect(screen.getByText('Content 2')).toBeVisible()
   })
-  // it('renders the summary and content', () => {
-  //   render(
-  //     <Details title="Click me">
-  //       <p>Some content</p>
-  //     </Details>
-  //   )
 
-  //   const summaryElement = screen.getByText('Click me')
-  //   const contentElement = screen.getByText('Some content')
+  it('toggles the accordion item when clicked', () => {
+    render(<Accordion list={list} />)
 
-  //   expect(summaryElement).toBeInTheDocument()
-  //   expect(contentElement).toBeInTheDocument()
-  // })
+    // Click on the first accordion item
+    fireEvent.click(screen.getByText('Title 1'))
 
-  // it('expands and collapses the content when clicked', () => {
-  //   render(
-  //     <Details title="Click me">
-  //       <p>Some content</p>
-  //     </Details>
-  //   )
+    // Assert that the first item is now open
+    expect(screen.getByText('Content 1')).toBeVisible()
 
-  //   const summaryElement = screen.getByText('Click me')
-  //   const contentElement = screen.getByText('Some content')
+    // Click on the second accordion item
+    fireEvent.click(screen.getByText('Title 2'))
 
-  //   expect(contentElement).not.toBeVisible()
-
-  //   summaryElement.click()
-
-  //   expect(contentElement).toBeVisible()
-
-  //   summaryElement.click()
-
-  //   expect(contentElement).not.toBeVisible()
-  // })
+    // Assert that the first item is closed and the second item is open
+    expect(screen.queryByText('Content 1')).not.toBeVisible()
+    expect(screen.getByText('Content 2')).toBeVisible()
+  })
 })
